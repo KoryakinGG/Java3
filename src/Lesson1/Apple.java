0d48a8ba2fcedffc2c3a83a15da62d8da2d5c93a
KoryakinGG <koryaking@mail.ru>
2020-04-04 16:26:48 +0300

    HomeTasks is done

--- src/Lesson1/Apple.java
+++ src/Lesson1/Apple.java
@@ -1,1 +1,9 @@
+package Lesson1;
 
+public class Apple extends Fruit{
+
+    @Override
+    float getWeight() {
+        return 1.0f;
+    }
+}
--- src/Lesson1/Orange.java
+++ src/Lesson1/Orange.java
@@ -1,1 +1,9 @@
+package Lesson1;
 
+public class Orange extends Fruit{
+
+    @Override
+    float getWeight() {
+        return 1.5f;
+    }
+}
