6c8f1ef6ad50de9c753112bf9db7a8efa2b4a4dd
KoryakinGG <koryaking@mail.ru>
2020-04-08 14:42:24 +0300

    Initial commit

---
+++ Maven1/src/main/java/Lesson2/DataBase.java
@@ -0,0 +1,12 @@
+package Lesson2;
+
+import javax.xml.crypto.Data;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/*CREATE, UPDATE, DELETE, INSERT, SELECT*/
+
+public class DataBase {
+
+}
---
+++ Maven1/src/main/java/Lesson2/Main.java
@@ -0,0 +1,8 @@
+package Lesson2;
+
+import java.sql.SQLException;
+
+public class Main extends DataBase{
+
+
+}
