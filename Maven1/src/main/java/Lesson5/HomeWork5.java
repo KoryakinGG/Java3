ea8fdbaaba0499edf5747229006924c80cb8968b
KoryakinGG <koryaking@mail.ru>
2020-04-19 01:16:02 +0300

    Сделанная домашняя работа

--- Maven1/src/main/java/Lesson5/HomeWork5.java
+++ Maven1/src/main/java/Lesson5/HomeWork5.java
@@ -1,4 +1,23 @@
 package Lesson5;
 
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class HomeWork5 {
+    public static final int CARS_COUNT = 4;
+    public static final int HALF_CARS_COUNT = CARS_COUNT/2;
+    public static CyclicBarrier cyclicBarrier = new CyclicBarrier(CARS_COUNT, new StartRaceInfo());
+
+    public static void main(String[] args) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+        }
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+        }
+    }
 }
